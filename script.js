a59f6d2088f0469ee25ecd8521718f1314bcb138
Syed Amin Al Mahim <hasibmahim@gmail.com>
2018-05-23 03:25:47 +0000

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-/* JavaScript code here */
+//Task 5
+var cart = [ 
+   {
+       name: 'Shoes',
+       price: 560,
+       quantity: 4
+   },
+   {
+       name: 'Regular Tees',
+       price: 455.50,
+       quantity: 6
+   },
+   {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   }];
+
+function addItem(newItem)
+{
+    cart.push(newItem);
+    console.log(cart);
+}
+
+var newItem = {name: 'Watch',price: 500.50,quantity: 5};
+addItem(newItem);
+
+
+//task 5b
+function sortCart(keyName)
+{
+    cartA(keyname)
+}
